508a54ca8c03bb15e9a6365a7ffc4136697a083f
Adam Dybcio <adam.dybcio.kontakt@gmail.com>
2026-08-24 17:21:11 +0200

    Add exception handling for malformed request bodies and update payment method type in Stripe service

--- src/main/java/pl/dybcio/ordered/common/exception/GlobalExceptionHandler.java
+++ src/main/java/pl/dybcio/ordered/common/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package pl.dybcio.ordered.common.exception;
 import java.util.stream.Collectors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -20,6 +21,12 @@ public class GlobalExceptionHandler {
             .map(fe -> fe.getField() + ": " + fe.getDefaultMessage())
             .collect(Collectors.joining("; "));
     return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, message);
+  }
+
+  @ExceptionHandler(HttpMessageNotReadableException.class)
+  public ProblemDetail handleUnreadableBody(HttpMessageNotReadableException ex) {
+    return ProblemDetail.forStatusAndDetail(
+        HttpStatus.BAD_REQUEST, "Request body is malformed or unreadable");
   }
 
   @ExceptionHandler(ProductNotPurchasedException.class)
